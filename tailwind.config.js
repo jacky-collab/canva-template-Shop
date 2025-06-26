267dae6e9bc37b4be163e4a1123321aef0fb5929
jacky-collab <grafikdesign.js22@gmail.com>
2025-06-27 00:05:50 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{js,ts,jsx,}", "./components/**/*.{js,ts,jsx,https://payhip.com/b/XW8mL}"],
   theme: { extend: {} },
   plugins: [],
 };
